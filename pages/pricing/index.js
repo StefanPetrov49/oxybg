9dba706be6b5c8ffd71351d5018be85efdf5a28c
StefanPetrov49 <stefanpetrov49@gmail.com>
2025-03-16 03:44:09 +0200

    Add feedback changes

---
+++ pages/pricing/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout/Layout";
+import Pricing from "../../components/Pricing";
+
+
+export default function Home() {
+  return (
+    <Layout>
+      <Pricing></Pricing>
+    </Layout>
+  );
+}
